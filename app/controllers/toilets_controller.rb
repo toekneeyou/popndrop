727c8ce7520cf6bfe655a8f91bf3771be1efdf6a
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-05-27 17:40:32 +0800

    Revert "created 4 controllers"

--- app/controllers/toilets_controller.rb
+++
@@ -1,2 +0,0 @@
-class ToiletsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
-  resources :toilets
-    resources :users
-    resources :bookings
-    resources :reviews
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
