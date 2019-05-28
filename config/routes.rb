403a7530b2937a8e439876978e5b64b25bb4475b
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-05-28 12:52:24 +0800

    updated routes and navbar for functional logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :toilets do
     resources :bookings, only: [:create]
