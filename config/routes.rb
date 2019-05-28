a1b202af757c6be059f03d6366312eda93e672b9
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-05-28 09:06:52 +0800

    cool

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :toilets
-  resources :users
-  resources :bookings
-  resources :reviews
+  resources :toilets do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy, :update] do
+      resources :reviews, only: [:create, :index, :show]
+  end
+
+  namespace :my do
+    resources :toilets, only: [:index]
+    resources :bookings, only: [:index, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
