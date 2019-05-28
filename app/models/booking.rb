80509f627d1dd45bd67b36d2563767684e4b1d53
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-05-28 11:20:52 +0800

    added the validations to the models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :toilet
   belongs_to :user
+  has_one :review
+
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook]
   mount_uploader :photo, PhotoUploader
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniquness: true
+  validates :encrypted_password, presence: true
+
+  has_many :bookings
+  has_many :reviews, throug: :bookings
 end
