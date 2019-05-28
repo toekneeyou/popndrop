f6d09dada8ef20b9bb5afb729e6558c6ee9fc3f8
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-05-28 11:44:34 +0800

    corrected typo

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,5 @@
 class ToiletsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new, :index]
+  # skip_before_action :authenticate_user!, only: [:new, :index]
   # before_action :set_toilet, only: [:index, :show, :new, :create]
 
   def index
