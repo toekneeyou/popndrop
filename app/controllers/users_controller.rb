3305b5944235fb271e72f2fe06db539bd6040c7f
Tony Yu <tonyyu1129@hotmail.com>
2019-05-31 12:20:05 +0800

    can edit profile now and registration info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to poopspace_user_path(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :photo)
+  end
 end
